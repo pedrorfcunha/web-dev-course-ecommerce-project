f41f6fe8147bf0773ae2986dae80d081496fd797
Pedro Cunha <pedrorfcunha@outlook.com>
2023-02-06 16:54:04 -0500

    project finished and reviewed

--- models/product.model.js
+++ models/product.model.js
@@ -1,6 +1,6 @@
-const mongodb = require("mongodb");
+const mongodb = require('mongodb');
 
-const db = require("../data/database");
+const db = require('../data/database');
 
 class Product {
   constructor(productData) {
@@ -25,11 +25,11 @@ class Product {
     }
     const product = await db
       .getDb()
-      .collection("products")
+      .collection('products')
       .findOne({ _id: prodId });
 
     if (!product) {
-      const error = new Error("Could not find product with provided id.");
+      const error = new Error('Could not find product with provided id.');
       error.code = 404;
       throw error;
     }
@@ -38,7 +38,23 @@ class Product {
   }
 
   static async findAll() {
-    const products = await db.getDb().collection("products").find().toArray();
+    const products = await db.getDb().collection('products').find().toArray();
+
+    return products.map(function (productDocument) {
+      return new Product(productDocument);
+    });
+  }
+
+  static async findMultiple(ids) {
+    const productIds = ids.map(function(id) {
+      return new mongodb.ObjectId(id);
+    })
+    
+    const products = await db
+      .getDb()
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray();
 
     return products.map(function (productDocument) {
       return new Product(productDocument);
@@ -66,14 +82,14 @@ class Product {
         delete productData.image;
       }
 
-      await db.getDb().collection("products").updateOne(
+      await db.getDb().collection('products').updateOne(
         { _id: productId },
         {
           $set: productData,
         }
       );
     } else {
-      await db.getDb().collection("products").insertOne(productData);
+      await db.getDb().collection('products').insertOne(productData);
     }
   }
 
@@ -84,7 +100,7 @@ class Product {
 
   remove() {
     const productId = new mongodb.ObjectId(this.id);
-    return db.getDb().collection("products").deleteOne({ _id: productId });
+    return db.getDb().collection('products').deleteOne({ _id: productId });
   }
 }
 
