3e47c5395aa04b8ffd71d2d8ed89e4d0774062d6
Pedro Cunha <pedrorfcunha@outlook.com>
2023-02-07 15:14:25 -0500

    payments demo added

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,10 +1,14 @@
-const Order = require('../models/order.model');
-const User = require('../models/user.model');
+const stripe = require("stripe")(
+  "sk_test_123"
+);
+
+const Order = require("../models/order.model");
+const User = require("../models/user.model");
 
 async function getOrders(req, res) {
   try {
     const orders = await Order.findAllForUser(res.locals.uid);
-    res.render('customer/orders/all-orders', {
+    res.render("customer/orders/all-orders", {
       orders: orders,
     });
   } catch (error) {
@@ -13,6 +17,8 @@ async function getOrders(req, res) {
 }
 
 async function addOrder(req, res, next) {
+  const cart = res.locals.cart;
+
   let userDocument;
   try {
     userDocument = await User.findById(res.locals.uid);
@@ -31,10 +37,38 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  res.redirect('/orders');
+  const session = await stripe.checkout.sessions.create({
+    line_items: cart.items.map(function (item) {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.product.title,
+          },
+          unit_amount: +item.product.price.toFixed(2) * 100,
+        },
+        quantity: item.quantity,
+      };
+    }),
+    mode: "payment",
+    success_url: `http://localhost:3000/orders/success`,
+    cancel_url: `http://localhost:3000/orders/failure`,
+  });
+
+  res.redirect(303, session.url);
+}
+
+function getSuccess(req, res) {
+  res.render("customer/orders/success");
+}
+
+function getFailure(req, res) {
+  res.render("customer/orders/failure");
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getFailure: getFailure,
+  getSuccess: getSuccess,
 };
--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,7 @@
-const bcrypt = require("bcryptjs");
-const mongodb = require("mongodb");
+const bcrypt = require('bcryptjs');
+const mongodb = require('mongodb');
 
-const db = require("../data/database");
+const db = require('../data/database');
 
 class User {
   constructor(email, password, fullname, street, postal, city) {
@@ -15,17 +15,17 @@ class User {
     };
   }
 
-  static findbyId(userId) {
+  static findById(userId) {
     const uid = new mongodb.ObjectId(userId);
 
     return db
       .getDb()
-      .collection("users")
+      .collection('users')
       .findOne({ _id: uid }, { projection: { password: 0 } });
   }
 
   getUserWithSameEmail() {
-    return db.getDb().collection("users").findOne({ email: this.email });
+    return db.getDb().collection('users').findOne({ email: this.email });
   }
 
   async existsAlready() {
@@ -39,9 +39,9 @@ class User {
   async signup() {
     const hashedPassword = await bcrypt.hash(this.password, 12);
 
-    await db.getDb().collection("users").insertOne({
+    await db.getDb().collection('users').insertOne({
       email: this.email,
-      password: hashedPassword, // ?
+      password: hashedPassword,
       name: this.name,
       address: this.address,
     });
